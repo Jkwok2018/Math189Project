62ff15fecd27a0e47ae569d9d8bea943281298ef
Jkwok2018 <jessica.kwok19@gmail.com>
2020-03-09 20:29:18 -0700

    PCA and plotting done

--- testEigenVector.py
+++ testEigenVector.py
@@ -3,27 +3,28 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
+import math as math
 
+# read in the data, X will be in the form [[x1,y1],[..],...]
 d  = pd.read_csv("test1_change_only.csv")
 JAPAN = d['JAPAN']
 HK = d['HK']
 dataframe = d.loc[:, ['JAPAN', 'HK']]
 X = np.array(dataframe.to_numpy())
+
+# Perform PCA
 pca = PCA()
+pca.fit(X)
 # Covariance matrix
 # [[0.67028827 0.08640905]
 # [0.08640905 1.6896526 ]]
 
-pca.fit(X)
-# print(pca.get_covariance())
-# print(pca.components_)
-# print(pca.explained_variance_)
-# plt.scatter(JAPAN, HK, color='r')
-# plt.xlabel('JAPAN')
-# plt.ylabel('HK')
-# plt.savefig("Japan VS HK")
+y = pca.components_[0][1]
+x = pca.components_[0][0]
+evals = pca.explained_variance_
+# [1.6969254  0.66301547]
 
-
+# Draw the eignevectors
 def draw_vector(v0, v1, ax=None):
     ax = ax or plt.gca()
     arrowprops=dict(arrowstyle='->',
@@ -33,20 +34,14 @@ def draw_vector(v0, v1, ax=None):
     ax.annotate('', v1, v0, arrowprops=arrowprops)
 
 # plot data
-plt.scatter(JAPAN, HK, color='r')
+plt.scatter(X[:, 0], X[:, 1], alpha=0.2)
 for length, vector in zip(pca.explained_variance_, pca.components_):
-    print(vector)
     v = vector * 3 * np.sqrt(length)
     draw_vector(pca.mean_, pca.mean_ + v)
-
-
 plt.axis('equal')
+plt.margins(1,1)
+plt.scatter(JAPAN, HK, color='r')
 plt.show()
 
-
-# centered_matrix = X - X.mean(axis=1)[:, np.newaxis]
-# cov = np.dot(centered_matrix.T, centered_matrix)
-# # print(cov)
-# eigvals, eigvecs = np.linalg.eig(cov)
-# print(eigvecs)
-# print((eigvecs.real)[1])
+#  Calculate theta in degrees
+theta = math.atan(y/x)*180/(math.pi)
