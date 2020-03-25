8d1ed59c72cc7bac5962ded461e8c303757cf97f
Melody Zhao <mezhao@g.hmc.edu>
2020-03-25 16:18:57 -0700

    Preprocess code

--- preprocess.py
+++ preprocess.py
@@ -1,68 +1,57 @@
+
 import numpy as np
 import pandas as pd
 
+sp500  = pd.read_csv("SP500HistoricalData.csv") #Excel File
+jpn = pd.read_csv("Japan.csv")
+hk = pd.read_csv("HSI.csv")
+sh = pd.read_csv("Shanghai.csv")
+sz = pd.read_csv("Shenzhen.csv")
 
-#Read in data from Excel Sheets
-sh  = pd.read_csv("ShanghaiTest.csv") #Excel File
-hk = pd.read_csv("HKTest.csv")
 sampleDate = pd.read_csv("SampleDate.csv")
 
-SHANGHAI = sh['PercentChange'] #Associated with Column Name
-SHANGHAIDate = sh['Date']
+spDate = sp500['Time'].tolist()
+spChange = sp500['Percent'].tolist()
 
-HK = hk['PercentChange']
-HKdate = hk['Date']
+jpnDate = jpn['Time'].tolist()
+jpnChange = jpn['Percent'].tolist()
 
-standardDate = sampleDate["Date"]
+hkDate = hk['Time'].tolist()
+hkChange = hk['Percent'].tolist()
+
+shDate = sh['Time'].tolist()
+shChange = sh['Percent'].tolist()
+
+szDate = sz['Date'].tolist()
+szChange = sz['Percent'].tolist()
+
+standardDate = sampleDate['Date'].tolist()
 export = []
-
-# print(standardDate[3])
-# print(SHANGHAIDate[0])
-# print(standardDate[3]==SHANGHAIDate[0])
-
-# for stdDate in standardDate:
-#     print(stdDate in SHANGHAIDate)
-#     new = []
-#     if (stdDate in SHANGHAIDate):
-#         new.append(SHANGHAI[SHANGHAIDate.index(stdDate)])
-#     else:
-#         new.append(300)
-#     if (stdDate in HKdate):
-#         new.append(HK[HKdate.index(stdDate)])
-#     else:
-#         new.append(300)
-#     export.append(new)
-
-# print(np.matrix(export))
-# print(SHANGHAIDate)
 
 for i in range(len(standardDate)):
     new = []
-    # print(standardDate[i])
-    if (standardDate[i] in SHANGHAIDate):
-        new.append(SHANGHAI[SHANGHAIDate.index(standardDate[i])])
+    new.append(standardDate[i])
+    if (standardDate[i] in spDate):
+        new.append(spChange[spDate.index(standardDate[i])])
     else:
-        new.append(300)
-    if (standardDate[i] in HKdate):
-        new.append(HK[HKdate.index(standardDate[i])])
+        new.append(0)
+    if (standardDate[i] in jpnDate):
+        new.append(jpnChange[jpnDate.index(standardDate[i])])
     else:
-        new.append(300)
+        new.append(0)
+    if (standardDate[i] in hkDate):
+        new.append(hkChange[hkDate.index(standardDate[i])])
+    else:
+        new.append(0)
+    if (standardDate[i] in shDate):
+        new.append(shChange[shDate.index(standardDate[i])])
+    else:
+        new.append(0)
+    if (standardDate[i] in szDate):
+        new.append(szChange[szDate.index(standardDate[i])])
+    else:
+        new.append(0)
     export.append(new)
 
-# print( str(standardDate[0]) == "2/21/15")
-# print( str(SHANGHAIDate[0]) == "2/25/15")
-
-# print(standardDate.index(str(SHANGHAIDate[0])))
-
-# x= sampleDate.index[sh.str.match(SHANGHAI[0])]
-# print(x)
-
-# print(SHANGHAIDate[0] in sampleDate.values)
-# print(sampleDate.index(SHANGHAIDate[0]))
-
-for item in SHANGHAIDate:
-    item = str(item)
-for item in standardDate:
-    item = str(item)
-print(SHANGHAIDate[0] in sampleDate.values)
-print("2/25/15" in sampleDate.values)
+df = pd.DataFrame(export, columns=['Date', "USA","Japan", "HK", "Shanghai", "Shenzhen"])    
+df.to_excel(r'test2.xlsx', index = False, header = True)
