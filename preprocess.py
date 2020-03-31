ad7e6cef3d3b0e93a9196bbbc1b9d6c758d94838
Melody Zhao <mezhao@g.hmc.edu>
2020-03-30 23:38:07 -0700

    Changed preprocessing to match current struct

--- preprocess.py
+++ preprocess.py
@@ -2,7 +2,31 @@
 import numpy as np
 import pandas as pd
 
-sp500  = pd.read_csv("SP500HistoricalData.csv") #Excel File
+sampleDate = pd.read_csv("SampleDate.csv")
+sp500  = pd.read_csv("S&PUpdated.csv") #Excel File
+
+spDate = sp500['Date'].tolist()
+spPriceChange = sp500['PriceChange'].tolist()
+spVolumeChange = sp500['VolumeChange'].tolist()
+
+standardDate = sampleDate['Date'].tolist()
+export = []
+
+for i in range(len(standardDate)):
+    new = []
+    new.append(standardDate[i])
+    if (standardDate[i] in spDate):
+        new.append(spPriceChange[spDate.index(standardDate[i])])
+        new.append(spVolumeChange[spDate.index(standardDate[i])])
+    else:
+        new.append(0)
+        new.append(0)
+    export.append(new)    
+
+df = pd.DataFrame(export, columns=['Date', 'Price Change', 'Volume Change'])    
+df.to_excel(r'newProcessed.xlsx', index = False, header = True)
+
+'''
 jpn = pd.read_csv("Japan.csv")
 hk = pd.read_csv("HSI.csv")
 sh = pd.read_csv("Shanghai.csv")
@@ -55,3 +79,5 @@ for i in range(len(standardDate)):
 
 df = pd.DataFrame(export, columns=['Date', "USA","Japan", "HK", "Shanghai", "Shenzhen"])    
 df.to_excel(r'test2.xlsx', index = False, header = True)
+
+'''
