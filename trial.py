5e160cf3387bdf278283b49b75858be5da9e5490
Melody Zhao <mezhao@g.hmc.edu>
2020-04-11 17:34:03 -0700

    Midterm Project submit

--- trial.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-raw_data = np.loadtxt("trial.txt", dtype=np.float32,
-    delimiter=",", skiprows=0, usecols=[0,1,2,3])
-
-print(raw_data)
-print(raw_data[0])
-print(type(raw_data))
